ce82379610ba9c0a81774557396327eb6aeb9191
JacopoFB <jacopofb03@gmail.com>
2026-03-11 12:06:38 -0600

    Services Sidebar component

---
+++ components/ServicesSidebar.js
@@ -0,0 +1,61 @@
+class ServicesSidebar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <aside class="sp-sidebar">
+                <div class="sb-widget">
+                    <div class="sb-widget-title">Our Services</div>
+                    <ul class="sb-tree">
+                        <li>
+                            <a href="./custom-decks.html" style="font-weight:600;color:var(--fg);">Decks</a>
+                            <ul class="sub">
+                                <li>
+                                    <a href="./custom-decks.html" class="sb-deck-link" data-service="custom-decks">
+                                        Custom Decks <i class="fas fa-chevron-right"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="sb-deck-link" data-service="composite-decks">
+                                        Composite Decks <i class="fas fa-chevron-right"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="sb-deck-link" data-service="wood-decks">
+                                        Wood Decks <i class="fas fa-chevron-right"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="./pergolas-covered-patios.html">
+                                Pergolas &amp; Covered Patios <i class="fas fa-chevron-right"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./outdoor-kitchens-firepits.html">
+                                Outdoor Kitchens &amp; Firepits <i class="fas fa-chevron-right"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./stamped-concrete-services.html">
+                                Stamped Concrete <i class="fas fa-chevron-right"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="sb-cta">
+                    <h3>Ready to Start?</h3>
+                    <p>Free on-site quote · No obligation</p>
+                    <a href="#contact" class="sb-cta-btn">Get a Free Quote</a>
+                    <a href="tel:+13039488821" class="sb-cta-phone"><i class="fas fa-phone"></i> 303-948-8821</a>
+                </div>
+            </aside>
+        `;
+    }
+}
+
+customElements.define("services-sidebar", ServicesSidebar);
+
+
+
+
+
